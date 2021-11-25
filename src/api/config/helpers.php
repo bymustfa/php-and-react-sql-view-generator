27d40f8fa54f2912dfd7ea9ef7e6b35cd2808395
Mustafa ÖZTÜRK <bay.mustafa.37@hotmail.com>
2021-11-26 00:16:05 +0300

    Php get databases

--- src/api/config/helpers.php
+++ src/api/config/helpers.php
@@ -10,6 +10,31 @@ function response($content = [], $status_code = 200)
     $response->send();
 }
 
+
+function sendMessage( $status, $datas ){
+    return ["status" => $status, "datas" => $datas];
+}
+
+// return guid
+function guid()
+{
+    if (function_exists('com_create_guid')) {
+        return com_create_guid();
+    } else {
+        mt_srand((double)microtime() * 10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);
+        $uuid = substr($charid, 0, 8) . $hyphen
+            . substr($charid, 8, 4) . $hyphen
+            . substr($charid, 12, 4) . $hyphen
+            . substr($charid, 16, 4) . $hyphen
+            . substr($charid, 20, 12);
+        return $uuid;
+    }
+}
+
+
+
 function dataClear($data)
 {
     if (is_array($data)) {
@@ -26,13 +51,13 @@ function post($name)
 {
     if(isset($_POST[$name]))
     {
-        if (is_array($_POST)) {
-            foreach ($_POST as $key => $value) {
-                $_POST[$key] = $value ? dataClear($value) : "";
+        if (is_array($_POST[$name])) {
+            foreach ($_POST[$name] as $key => $value) {
+                $_POST[$name][$key] = $value ? dataClear($value) : "";
             }
-            return $_POST;
+            return $_POST[$name];
         } else {
-            return $_POST ? dataClear($_POST) : "";
+            return $_POST[$name] ? dataClear($_POST[$name]) : "";
         } 
     } else{
         return null;
@@ -42,16 +67,17 @@ function post($name)
 
 
 function get($name)
-{
+{ 
+
     if(isset($_GET[$name]))
     {
-        if (is_array($_GET)) {
-            foreach ($_GET as $key => $value) {
-                $_GET[$key] = $value ? dataClear($value) : "";
+        if (is_array($_GET[$name])) {
+            foreach ($_GET[$name] as $key => $value) {
+                $_GET[$name][$key] = $value ? dataClear($value) : "";
             }
-            return $_GET;
+            return $_GET[$name];
         } else {
-            return $_GET ? dataClear($_GET) : "";
+            return $_GET[$name] ? dataClear($_GET[$name]) : "";
         } 
     } else{
         return null;
--- src/api/index.php
+++ src/api/index.php
@@ -1,7 +1,11 @@
 <?php
-require __DIR__ . '/vendor/autoload.php'; 
+require __DIR__ . '/vendor/autoload.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 ob_start();
-date_default_timezone_set( 'Europe/Istanbul');
+date_default_timezone_set('Europe/Istanbul');
 
 use Buki\Router\Router;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,60 +18,110 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 header('Access-Control-Allow-Credentials: true');
 
-$router->group("/V1", function($router){
+$router->group("/V1", function ($router)
+{
 
-    $router->group( "/databes", function($router){
-        $router->get("/", function(){
-            response(["message" => "Welcome to databes API"]);
+    $router->group("/databes", function ($router)
+    {
+        $router->get("/", function ()
+        {
+            try
+            {
+
+                $json = file_get_contents(__DIR__ . '/database.json');
+                $json = json_decode($json, true);
+                if (!isset($json))
+                {
+                    throw new Exception("Json file is broken");
+                }
+
+                response(sendMessage(true, $json));
+
+            }
+            catch(Exception $e)
+            {
+                response(sendMessage(false, $e->getMessage()) , 400);
+            }
+
         });
 
-
-        $router->post( "/add", function(){
-            
-                });
-
-
-        } );
-        
-
-
-
-
-
-
-
-        $router->group( "/tables", function($router){
-            $router->get("/", function(){
-                $response = query("SELECT * FROM demo_table", [], "test");
-                response(  $response);
-            });
-    
-            $router->get("/table-columns/:string", function($tableName){
-                $tableName = dataClear($tableName);
-                $response = query("SHOW COLUMNS FROM  $tableName", [], "test");
-                
-    
-                response($response);
-            });
-            
-        } );
-
-
-
-
-
-
-
-    } );
-
-
+        $router->post("/add", function ()
+        {
   
+            try
+            {
+                $json = file_get_contents(__DIR__ . '/database.json');
+                $json = json_decode($json, true);
 
-
-
-
+                $datas["id"] = guid();
+                $datas["db_host"] = post("db_host");
+                $datas["db_name"] = post("db_name");
+                $datas["db_user"] = post("db_user");
+                $datas["db_password"] = post("db_password");
  
+
+                if (!isset($datas['db_password']))
+                {
+                    $datas["db_password"] = "";
+                }
+
+                if (!isset($datas['db_host']))
+                {
+                    throw new Exception("db_host is required");
+                }
+                else if (!isset($datas['db_name']))
+                {
+                    throw new Exception("db_name is required");
+                }
+                else if (!isset($datas['db_user']))
+                {
+                    throw new Exception("db_user is required");
+                }
+                else
+                {
+                    $merge = array_merge($json, [$datas]);
+                    $saveDatas = json_encode($merge);
+
+                    if (file_put_contents(__DIR__ . '/database.json', $saveDatas))
+                    {
+                        response(sendMessage(true, "Database added successfully"));
+                    }
+                    else
+                    {
+                        throw new Exception("Database added failed");
+                    }
+                }
+
+            }
+            catch(Exception $e)
+            {
+                response(sendMessage(false, $e->getMessage()) , 400);
+            }
+
+        });
+
+    });
+
+    $router->group("/tables", function ($router)
+    {
+        $router->get("/", function ()
+        {
+            $response = query("SELECT * FROM demo_table", [], "test");
+            response($response);
+        });
+
+        $router->get("/table-columns/:string", function ($tableName)
+        {
+            $tableName = dataClear($tableName);
+            $response = query("SHOW COLUMNS FROM  $tableName", [], "test");
+
+            response($response);
+        });
+
+    });
+
+});
 
 $router->run();
- 
+
 ?>
